f52738efc167454d92508fc2bf0a8c769d80bf3d
Mani Movassagh <mani.mgh@gmail.com>
2022-09-09 14:15:53 +0200

    define request mapping

--- product-service/src/main/java/com/github/manimovassagh/product/controller/ProductController.java
+++ product-service/src/main/java/com/github/manimovassagh/product/controller/ProductController.java
@@ -4,9 +4,11 @@ package com.github.manimovassagh.product.controller;
 import com.github.manimovassagh.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
+@RequestMapping("/api/v1")
 public class ProductController {
     @Autowired
     ProductService productService;
