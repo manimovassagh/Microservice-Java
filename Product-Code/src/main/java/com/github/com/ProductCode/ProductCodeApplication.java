05f7eb98286af204c77efb4ee46c4c2ecb45ce24
Mani Movassagh <mani.mgh@gmail.com>
2022-09-10 13:27:08 +0200

    start

---
+++ Product-Code/src/main/java/com/github/com/ProductCode/ProductCodeApplication.java
@@ -0,0 +1,13 @@
+package com.github.com.ProductCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCodeApplication.class, args);
+	}
+
+}
---
+++ Product-Code/src/test/java/com/github/com/ProductCode/ProductCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.com.ProductCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
